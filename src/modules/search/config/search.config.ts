84d6ed01fc3de4641653ad7c2b717f6fef69e4b3
Muhammad Alhajee <alhajee2009@gmail.com>
2024-05-22 07:42:16 +0100

    feat: api elasticsearch config

--- src/modules/search/config/search.config.ts
+++ src/modules/search/config/search.config.ts
@@ -2,6 +2,10 @@ export class SearchConfig {
   public static searchConfig(url: string): any {
     return {
       node: url,
+      auth: {
+        username: process.env.ELASTICSEARCH_USERNAME,
+        password: process.env.ELASTICSEARCH_PASSWORD,
+      },
       maxRetries: 5,
       requestTimeout: 60000,
       sniffOnStart: true,
