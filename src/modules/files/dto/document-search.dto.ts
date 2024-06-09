96f35c7e1bf892a47a73765c6ad9ea5f263cb5ae
Muhammad Alhajee <alhajee2009@gmail.com>
2024-06-09 05:06:05 +0100

    feat: document search dto

--- src/modules/files/dto/document-search.dto.ts
+++ src/modules/files/dto/document-search.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class DocumentSearchDTO {
   @ApiProperty({
     description: 'Search query',
   })
+  @IsNotEmpty()
   readonly q: string;
 }
