40ffe162e5e04d61097290b63fca56e6bf5303f9
Muhammad Alhajee <alhajee2009@gmail.com>
2024-06-21 00:19:10 +0100

    feat: project entity

---
+++ src/modules/project/entities/project-base-entity.ts
@@ -0,0 +1,39 @@
+import { Exclude, Expose } from 'class-transformer';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import ProjectEntity from './project-entity';
+import { ProjectCategory } from '@prisma/client';
+
+@Exclude()
+export default class ProjectBaseEntity extends PartialType(ProjectEntity) {
+  @ApiProperty({ type: String })
+  @Expose()
+  readonly id: string;
+
+  @ApiProperty({ type: String, nullable: true })
+  @Expose()
+  readonly name: string | null;
+
+  @ApiProperty({ enum: ProjectCategory, nullable: true })
+  @Expose()
+  readonly category: ProjectCategory | null;
+
+  @ApiProperty({ type: String, nullable: true })
+  @Expose()
+  readonly description: string | null;
+
+  @ApiProperty({ type: Array, nullable: true })
+  @Expose()
+  readonly projectManagers: string[] | null;
+
+  @ApiProperty({ type: Array, nullable: true })
+  @Expose()
+  readonly projectMembers: string[] | null;
+
+  @ApiProperty({ type: Date, nullable: true })
+  @Expose()
+  readonly createdAt: Date | null;
+
+  @ApiProperty({ type: Date, nullable: true })
+  @Expose()
+  readonly updatedAt: Date | null;
+}
---
+++ src/modules/project/entities/project-entity.ts
@@ -0,0 +1,24 @@
+import { Project, ProjectCategory } from '@prisma/client';
+
+export default class ProjectEntity implements Project {
+  readonly id!: string;
+
+  readonly name!: string | null;
+
+  readonly description!: string | null;
+
+  readonly projectManagers!: string[] | null;
+
+  readonly projectMembers!: string[] | null;
+
+  readonly createdAt!: Date;
+
+  readonly updatedAt!: Date;
+
+  readonly category: ProjectCategory;
+  readonly tags: string[];
+  readonly createdByUserId: string;
+  readonly updatedByUserId: string;
+  readonly projectMembersIDs: string[];
+  readonly projectManagersIDs: string[];
+}
