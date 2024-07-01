5ab3c3342921505ebc19de299d6634fc7f047f95
Muhammad Alhajee <alhajee2009@gmail.com>
2024-07-01 23:52:52 +0100

    refactor: email service approval and disapproval email

--- src/modules/mail/services/mail.service.ts
+++ src/modules/mail/services/mail.service.ts
@@ -16,7 +16,7 @@ export class MailService {
     this.contactEmail = this.configService.get(CONTACT_EMAIL);
   }
 
-  async sendOTPConirmation(to: string, context?: any) {
+  async sendOTPConfirmation(to: string, context?: any) {
     try {
       await this.mailerService.sendMail({
         to,
@@ -31,6 +31,75 @@ export class MailService {
     } catch (error) {
       Logger.error('Error sending email', error.message);
     }
+  }
+
+  async sendApprovalRequestNotification(
+    to: string[] | string,
+    context: {
+      projectName: string;
+      documentName: string;
+      requestedBy: string;
+      submissionDate: string;
+      reviewLink: string;
+    },
+  ) {
+    try {
+      await this.mailerService.sendMail({
+        to,
+        subject: 'Approval Request Notification',
+        template: './pending-approval',
+        context: {
+          ...context,
+          year: new Date().getFullYear(),
+          website: this.siteUrl,
+        },
+      });
+    } catch (error) {
+      Logger.error('Error sending email', error.message);
+    }
+  }
+
+  async sendApprovalNotification(
+    to: string,
+    details: {
+      documentName: string;
+      approvalDate: string;
+      requestedBy: string;
+      projectLink: string;
+    },
+  ) {
+    await this.mailerService.sendMail({
+      to,
+      subject: 'Your Document Has Been Approved',
+      template: './approval-notification',
+      context: {
+        ...details,
+        year: new Date().getFullYear(),
+        website: this.siteUrl,
+      },
+    });
+  }
+
+  async sendDisapprovalNotification(
+    to: string,
+    details: {
+      documentName: string;
+      disapprovalDate: string;
+      disapprovalReason: string;
+      requestedBy: string;
+      projectLink: string;
+    },
+  ) {
+    await this.mailerService.sendMail({
+      to,
+      subject: 'Your Document Has Been Disapproved',
+      template: './disapproval-reason',
+      context: {
+        ...details,
+        year: new Date().getFullYear(),
+        website: this.siteUrl,
+      },
+    });
   }
 
   async sendPasswordResetEmail(to: string, context?: any) {
